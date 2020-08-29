f71a35d88ca7117e18ad548eef84a7683e069960
Sayantan Das <sayantanapril99@gmail.com>
2020-08-29 18:48:58 +0530

    better scrolling

--- src/components/GalleryHorizontal/GalleryHorizontal.jsx
+++ src/components/GalleryHorizontal/GalleryHorizontal.jsx
@@ -4,28 +4,28 @@ import "./GalleryHorizontal.css";
 const GalleryHorizontal = (component) => {
     const scrollArea = useRef();
 
-    useEffect(()=>{
-        scrollArea.current.scrollLeft += 20;
-    },[]);
+    // useEffect(()=>{
+    //     scrollArea.current.scrollLeft += 20;
+    // },[]);
 
     const onLeft = () => {
         let current_pos =  scrollArea.current.scrollLeft;
-        let scroll_ammount = scrollArea.current.clientWidth;
+        let scroll_ammount = scrollArea.current.clientWidth - 150;
         scrollArea.current.scrollTo({left: current_pos - scroll_ammount, behavior: "smooth"});
     }
     const onRight = () => {
         let current_pos =  scrollArea.current.scrollLeft;
-        let scroll_ammount = scrollArea.current.clientWidth;
+        let scroll_ammount = scrollArea.current.clientWidth - 150;
         scrollArea.current.scrollTo({left: current_pos + scroll_ammount, behavior: "smooth"});
     }
 
     return ( 
         <div className="gallery-h">
-            <button onClick={onLeft}>{"<"}</button>
+            <button onClick={onLeft}><div className="gallery-left-arrow"></div></button>
             <div ref={scrollArea} className="gallery-h-scrollarea">               
                 {component}
             </div>
-            <button onClick={onRight}>{">"}</button>
+            <button onClick={onRight}><div className="gallery-right-arrow"></div></button>
         </div>
     );
 }
